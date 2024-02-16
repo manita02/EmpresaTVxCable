49d04e9be5acd16b8fb355665971719e0ec61dd6
Ana Juarez <manitacoqui@gmail.com>
2024-02-16 12:52:22 -0300

    upload files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmpresaTVxCable.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
